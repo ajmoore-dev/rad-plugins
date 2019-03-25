4bde7ec4731bad7f266cfe04c59279db704ae01b
AJ Moore <truckfondue@gmail.com>
2019-03-25 09:20:09 -0600

    CLEANUP: removes functionality built into rad-shell

--- rad-truckfondue-plugin.plugin.zsh
+++ rad-truckfondue-plugin.plugin.zsh
@@ -1,23 +1,22 @@
 # ~/.zshrc
-### z - Opens ~/.zshrc
-alias z="subl /Users/alex.moore/.zshrc"
-### src - Apply changes to ~/.zshrc
-alias src="source ~/.zshrc"
+### .rc - Opens ~/.zshrc
+alias .rc="code $HOME/.zshrc"
+### s.rc - Apply changes to ~/.zshrc
+alias s.rc="source ~/.zshrc"
 
 
 # editors
-alias sm="emacs -nw"
+alias c="code -nw"
 alias s="subl ."
-alias jn="jupyter notebook"
 
 
 # search
-### gh - Search history
-alias gh="history | grep -i"
-### ag - Search aliases
-alias ag="alias | grep -i"
-### eg - Search ENV_VARs
-alias eg="env | grep -i"
+### eh - Search history
+alias eh="history | grep -i"
+### ea - Search aliases
+alias ea="alias | grep -i"
+### ee - Search ENV_VARs
+alias ee="env | grep -i"
 
 
 # macOS
@@ -51,18 +50,3 @@ alias gst="git status"
 alias gsta="git add . && git stash"
 alias gump="git checkout master && git fetch upstream --prune && git rebase upstream/master && git push origin master"
 alias gclear="git stash clear"
-
-# navigation
-alias tf="cd ~/truckfondue"
-
-# docker
-alias dcd="docker-compose down -v"
-### dbash - exec into a running container
-### Example: dbash c157caf0538b
-dbash() { docker exec -it $1 bash }
-
-### dst - stop one or more running containers
-### Example: dst c157caf0538b f6d41f8ffd5d
-dst () {
-  docker stop "$@"
-}
